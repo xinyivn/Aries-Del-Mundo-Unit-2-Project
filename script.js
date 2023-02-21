166c4df0cc03f97c592c0ed11e18a0c52a2f7997
xinyivn <126012867+xinyivn@users.noreply.github.com>
2023-02-21 10:25:09 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,41 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let option1Button =
+    document.querySelector(".option1");
+let option1Screen = document.querySelector(".option-one-screen");
+
+option1Button.onclick=function(){
+	option1Screen.style.display="block";
+};
+
+let option2Button =
+    document.querySelector(".option2");
+let option2Screen = document.querySelector(".option-two-screen");
+
+option2Button.onclick=function(){
+    option2Screen.style.display="block";
+};
+
+let option3Button =
+    document.querySelector(".option3");
+let option3Screen = document.querySelector(".option-three-screen");
+
+option3Button.onclick=function(){
+	option3Screen.style.display="block";
+};
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+option1Button.onclick=function(){
+    option1Screen.style.display="block";
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
